acfd85e635abd15e2c67c54e30e36eb42796a73d
Serhii Libin <serhii.libin@datatrans.ch>
2026-04-27 16:35:23 +0200

    iOS: Docs

--- Datatrans.xcframework/ios-arm64/Datatrans.framework/Headers/DTErrorPresenter.h
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-
-@protocol DTErrorPresenter <NSObject>
-
-- (void)showCriticalErrorWithTitle:(nonnull NSString *)title message:(nonnull NSString *)message dismissAction:(void (^_Nonnull)(void))dismissAction; // ok button
-- (void)showErrorWithTitle:(nonnull NSString *)title message:(nonnull NSString *)message; // ok button
-- (void)showErrorWithTitle:(nonnull NSString *)title message:(nonnull NSString *)message dismissAction:(void (^_Nullable)(void))dismissAction retryAction:(void (^_Nullable)(void))retryAction; // ok or retry button (if action provided), cancel and retry button if both actions provided
-
-@end
